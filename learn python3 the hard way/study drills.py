1e1508e08f032ede23693b3bb4e58f70460ac27e
zhangfz <977644072@qq.com>
2018-10-11 15:46:00 +0800

    Update study drills.py

--- learn python3 the hard way/study drills.py
+++ learn python3 the hard way/study drills.py
@@ -1,1 +1,26 @@
 
+# ex0
+---
+
+* 退出指令
+quit（）     /    CTRL-Z (^Z)  /  exit（）
+PowerShell  命令行终端程序
+Terminal      终端
+
+* 新建目录
+new-item（新建目录）
+mkdir mystuff
+mkdir   建立一个新的子目录（DOS命令）mystuff  我的东西（目录名称）
+
+* 改变目录
+cd   有时也写作chdir（change directory，改变目录），是在Unix、Windows和DOS操作系统下用于改变工作目录的命令行命令。
+如何在已经进入某一个文件时再退出进入其他目录 输入cd E:\pthw(E必须为大写）
+
+* 其它
+path          路径
+Type         类型
+file            文件
+directory   目录
+dir （directory，目錄）是一個條列檔案及目錄的命令行界面指令
+Volume in drive C is ，Volume Serial Number is 085C-7E02
+驱动器C中的卷是，卷序列号为085C-7E02
