3e201142a3a46e7f8d24cc9c860cde23b9716bdc
zhangfz <977644072@qq.com>
2018-10-14 20:47:56 +0800

    Update ex9.py

--- learn python3 the hard way/ex9.py
+++ learn python3 the hard way/ex9.py
@@ -1,11 +1,11 @@
 # Here;s some new strange stuff, remember type it exactle.
-
+# 使用了换行转义字符
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\njul\nAng"
 
 print("Here are the days:", days)
 print("Here are the months:", months)
-
+# 使用三个双引号或单引号创建的字符串将保留原始额输入状态
 print("""
 There's something going on here.
 With the three double-quotes.
