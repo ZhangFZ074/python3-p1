dca5bd1214e3ab35365a38ab4a9d32ed0a435d86
zhangfz <977644072@qq.com>
2018-10-15 06:19:20 +0800

    Create ex10.py

---
+++ learn python3 the hard way/ex10.py
@@ -0,0 +1,20 @@
+# 转义字符的使用
+# 水平制表符
+tabby_cat = "\tI'm tabbed in."
+# 换行符
+peersian_cat = "\tI'm split\n\ton a line."
+# 反斜杠
+backslask_cat = "\tI'm \\ a \\ cat."
+
+# 使用三个双引号录入大段文字，使用制表符加星号制作目录
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print = (taby_cat)
+print = (persian_cat)
+print = (backslash_cat)
+peint = (fat_cat)
