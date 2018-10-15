510413076fc4bae912e6d98ff39f83a6d57dc530
zhangfz <977644072@qq.com>
2018-10-15 11:23:06 +0800

    Create ex12.py

---
+++ learn python3 the hard way/ex12.py
@@ -0,0 +1,12 @@
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print(f"I'm {age}old, {height} tall and {weight} heavy.")
+
+# 加一个格式化试试
+age = input("How old are you?")
+height = input(f"You're {age}? Nice. How tall aare you?")
+weight = input("How much do you weigh?")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
