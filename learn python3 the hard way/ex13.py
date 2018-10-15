3b94a14f3ed604b4c480d05ad0e3f0472fbc82a3
zhangfz <977644072@qq.com>
2018-10-16 06:59:18 +0800

    Create ex13.py

---
+++ learn python3 the hard way/ex13.py
@@ -0,0 +1,11 @@
+# 用 import 导入模块 sys
+from sys import argv 
+# read the WYSS section for how to run this
+# argv有四个值，将所有参数赋值给左边的变量
+scipt, first, secound, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your secnd variable is:", second)
+print("Your third cariable is:", third)
+
