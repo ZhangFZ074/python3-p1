7e4ef43d8c8c9e5670ebf7cbf1557679d583a76e
zhangfangzheng <zhangfangzheng5@gmail.com>
2019-04-27 22:10:37 +0800

    427

--- learn python3 the hard way/ex0.py
+++ learn python3 the hard way/ex0.py
@@ -1,10 +1,23 @@
-# 在powershell终端中运行python。
-python
-# 键入quit(),退出python。
-quit()
-# 在powershell中创建一个目录。
-mkdir lpthw
-# 在powershell中变到一个目录。
-cd lpthw
-# 在powershell中列出目录。
-dir
+'''
+python # 在powershell终端中运行python。
+quit() # 键入quit(),退出python。
+mkdir lpthw # 在powershell中创建一个目录。
+cd lpthw # 在powershell中变到一个目录。
+dir # 在powershell中列出目录。
+'''
+print("Hens", 25 + 30 / 6) # 数字不用加引号
+print("Roosters", 100 - 25 * 3 % 4) # 逗号是分隔的作用。
+print(3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6)
+# 是真（真）还是假（ false）呢？
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7) # False
+print("What is 3 + 2?", 3 + 2)
+print("Is it greater?", 5 > -2)
+print("Is it less or equal?", 5 <= -2)
+
+########### 变量 ###############
+cars = 100  # 变量
+drivers = 50
+print("There are", cars, "cars available.")
+cars_not_driven = cars - drivers  # 变量也可以运算
+print("There will be", cars_not_driven, "empty cars today.")
--- learn python3 the hard way/ex1.py
+++
@@ -1,8 +0,0 @@
-# 井号不显示、不读取、备注用，这是我的第一个程序。
-print("Hello World!")
-print("Hello Again")
-print("I like typing this.")
-print("This is fun.")
-print('Yay! printing.')
-print("I' d much rather you 'not'.")
-print('I "said" do not touch this.')
